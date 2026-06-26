0e4ee1e2681c72923f1f53e02f98f1505ef21c39
mayanktiwari101 <iammayanktheprogrammer@gmail.com>
2026-06-26 15:33:19 +0530

    pricingCardDone

--- script.js
+++ script.js
@@ -33,8 +33,24 @@ $(document).ready(function () {
             $('.filter').filter('.' + value).show(300);
         }
     });
+
     $('.gallery-list-item').click(function() {
         $(this).addClass('active-item').siblings().removeClass('active-item');
     });
+
+    $(window).scroll(function() {
+        let position = $(this).scrollTop();
+        if(position >= 4300) {
+            $('.card-1').addClass('moveFromLeft');
+            $('.card-2').addClass('moveFromBottom');
+            $('.card-3').addClass('moveFromRight');
+        } else {
+            $('.card-1').removeClass('moveFromLeft');
+            $('.card-2').removeClass('moveFromButton');
+            $('.card-3').removeClass('moveFromRight');
+        }
+    });
 });
+
+
 
